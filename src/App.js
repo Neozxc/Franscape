3d18583062116fcdb36a8d5863d092554ed47f1a
Nedas <neduska991@gmail.com>
2022-06-26 16:01:53 +0100

    Push

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+import SingleMovie from "./components/SingleMovie";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="movie/:id" element={<SingleMovie />} />
+    </Routes>
+  );
+};
+
+export default App;
